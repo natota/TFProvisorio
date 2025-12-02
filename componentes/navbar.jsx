aae3698d5a7d8a461dd9d32c952ec921575dd51b
Natalia <nataliagentili37@gmail.com>
2025-12-02 12:31:09 -0300

    barra navegación

--- componentes/navbar.jsx
+++ componentes/navbar.jsx
@@ -5,11 +5,16 @@ export default function Navbar() {
     return (
         <div className={estilo.nav}>
             <ul>
-                <li><Link className={estilo.navLink} href="./">Inicio</Link></li>
-                <li><Link className={estilo.navLink} href="/nosotros">Nosotros</Link></li>
-                <li><Link className={estilo.navLink} href="/servicios">Servicios</Link></li>
-                <li><Link className={estilo.navLink} href="/contacto">Contacto</Link></li>
+                <li> <Link className={estilo.navE} href="./">🏠</Link>
+                    <Link className={estilo.navLink} href="./">Inicio</Link>
+                </li>
+                <li><Link className={estilo.navE} href="/nosotros">👨‍👩‍👧‍👦</Link>
+                    <Link className={estilo.navLink} href="/nosotros">Nosotros</Link></li>
+                <li> <Link className={estilo.navE} href="/servicios">🐕‍🦺</Link>
+                    <Link className={estilo.navLink} href="/servicios">Servicios</Link></li>
+                <li><Link className={estilo.navE} href="/contacto">☎️</Link>
+                    <Link className={estilo.navLink} href="/contacto">Contacto</Link></li>
             </ul>
         </div>
-    )
+    );
 }
